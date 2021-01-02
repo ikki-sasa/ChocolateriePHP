103d6df43f749e1b9baa4f8953fb709ebe41469d
ikki-sasa <manga.samadjine@3wa.io>
2021-01-02 19:36:55 +0100

    slider ok

--- www/js/main.js
+++ www/js/main.js
@@ -1,49 +1,52 @@
 /*SLIDER*/
-const items = document.querySelectorAll('img');
+const items = document.querySelectorAll('.slider img');
 const nbSlide = items.length;
 const next = document.querySelector('.right');
 const preview = document.querySelector('.left');
 let count = 0;
 
-function slideNext() {          /*pour faire apparaitre slide suivante*/
-    items[count].classList.remove('active');
-    
-    if(count < nbSlide - 1){            
-        count++;                        /*passe l'image suivante */
-    } else {
-        count = 0;                         /*sinon ont repasse le compteur à 0 */
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    function slideNext() {          /*pour faire apparaitre slide suivante*/
+        items[count].classList.remove('active');
+        
+        if(count < nbSlide - 1){            
+            count++;                        /*passe l'image suivante */
+        } else {
+            count = 0;                         /*sinon ont repasse le compteur à 0 */
+        }
+        
+        items[count].classList.add('active');    /*ajout de la classe active */
+        console.log(count); 
+    }
+
+    function slidePreview() {                       /*Le contriare de la première fonction*/
+        items[count].classList.remove('active');
+        
+        if(count > 0) {
+            count--;
+        } else {
+            count = nbSlide -1
+        }
+        
+        items[count].classList.add('active');
     }
     
-    items[count].classList.add('active')    /*ajout de la classe active */
-    console.log(count); 
-}
-
-next.addEventListener('click', slideNext);
-
-
-function slidePreview() {                       /*Le contriare de la première fonction*/
-    items[count].classList.remove('active');
-    
-    if(count > 0) {
-        count--;
-    } else {
-        count = nbSlide -1
+    function keyPress(e) {
+        if(e.keyCode === 37) {
+            slidePreview();
+        } else if (e.keyCode === 39) {
+            slideNext();
+        }
     }
-    
-    items[count].classList.add('active');
-}
-preview.addEventListener('click', slidePreview);
 
+    var myvar = setInterval(slideNext, 3000);
+    next.addEventListener('click', slideNext);
+    preview.addEventListener('click', slidePreview);    
+    document.addEventListener('keydown', keyPress);
 
+});
 
-function keyPress(e) {
-    if(e.keyCode === 37) {
-        slidePreview();
-    } else if (e.keyCode === 39) {
-        slideNext();
-    }
-}
-document.addEventListener('keydown', keyPress)
 
 
 
